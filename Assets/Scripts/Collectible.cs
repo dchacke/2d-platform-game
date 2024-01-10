1afaca4f9beaeca59c1f62b61c0f0394523e8cfc
Dennis Hackethal <dh@dennishackethal.com>
2024-01-10 14:15:29 -0700

    Increase cherry count when collecting cherry

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    private GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class Collectible : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            gm.IncCherryCount();
         }
     }
 }
