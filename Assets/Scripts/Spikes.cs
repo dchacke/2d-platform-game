974865c3094ddd9b5ca0cd0396cb45532696d2a2
Dennis Hackethal <dh@dennishackethal.com>
2024-01-11 11:54:12 -0700

    Play hurt sound when stepping on spikes

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -6,17 +6,22 @@ public class Spikes : MonoBehaviour
 {
     PlayerController pc;
     GameManager gm;
+    AudioSource audioSource;
+
+    [SerializeField] AudioClip hurtSound;
 
     void Awake()
     {
         pc = GameObject.Find("Player").GetComponent<PlayerController>();
         gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && !pc.IsAscending())
         {
+            audioSource.PlayOneShot(hurtSound);
             gm.TriggerGameOver();
         }
     }
