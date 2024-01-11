8fac4cc821e738c5e4bfdb7442bb5a7444a00887
Dennis Hackethal <dh@dennishackethal.com>
2024-01-11 10:50:55 -0700

    Spawn spikes on top of some platforms

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     [SerializeField] GameObject cherryPrefab;
+    [SerializeField] GameObject spikesPrefab;
+
+    float width;
+    float height;
 
     // Start is called before the first frame update
     void Start()
     {
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+
         SpawnCherry();
+        SpawnSpikes();
     }
 
     // Update is called once per frame
@@ -22,14 +30,25 @@ public class Platform : MonoBehaviour
     {
         if (Random.Range(0.0f, 1.0f) < 0.2f)
         {
-            float width = GetComponent<SpriteRenderer>().bounds.size.x;
-            float height = GetComponent<SpriteRenderer>().bounds.size.y;
             float x = Random.Range(transform.position.x - width / 2, transform.position.x + width / 2);
             float y = transform.position.y + height + 0.25f;
 
             Vector3 pos = new Vector3(x, y, 0);
 
             Instantiate(cherryPrefab, pos, cherryPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnSpikes()
+    {
+        if (Random.Range(0.0f, 1.0f) < 1f)
+        {
+            float x = Random.Range(transform.position.x - width / 2, transform.position.x + width / 2);
+            float y = transform.position.y + height - 0.05f;
+
+            Vector3 pos = new Vector3(x, y, 0);
+
+            Instantiate(spikesPrefab, pos, cherryPrefab.transform.rotation);
         }
     }
 }
